5f09eb51d3132b78bfe9825abff2056af4ac6e41
Esteban Flores <ebflores88@gmail.com>
2020-02-21 04:25:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-var kittens = [] //define your array here
+var kittens = ['Milo', 'Otis', 'Garfield'] //define your array here
 
 // Add your functions and code here
+destructivelyAppendKitten = function(name){
+  kittens.push(name);
+  return kittens;
+}
+
+destructivelyPrependKitten = function(name){
+  kittens.unshift(name);
+  return kittens;
+}
+
+destructivelyRemoveLastKitten = function(){
+  kittens.pop();
+  return kittens;
+}
