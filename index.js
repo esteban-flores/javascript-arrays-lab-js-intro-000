212c78e269c0502590f8048318e47025188f9908
Esteban Flores <ebflores88@gmail.com>
2020-02-21 05:02:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ function destructivelyRemoveFirstKitten(){
 returns a new array, leaving the kittens array unchanged:*/
 var kittens = ['Milo', 'Otis', 'Garfield']
 function appendKitten(name){
-  var newKittens = [];
+  var newKittens = [''];
   newKittens = kittens.concat(newKittens.push(name));
   return newKittens;
 }
